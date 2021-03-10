d43f493a726a7d95675beae413a4a8a6c195c4d2
CFE <hello@teamcfe.com>
2021-03-10 13:46:11 -0800

    6 - Migrate your Django Model

--- src/products/models.py
+++ src/products/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(max_length=None)
-    description = models.TextField(null=True)
+    title = models.CharField(max_length=120)
+    # description = models.TextField(null=True)
     price = models.DecimalField(max_digits=20, decimal_places=2)
