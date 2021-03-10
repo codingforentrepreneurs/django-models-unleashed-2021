9fd4128331badec9fd85e0b8cc78d9e5477f9a46
CFE <hello@teamcfe.com>
2021-03-10 13:54:46 -0800

    7 - Make Changes to Models

--- src/products/models.py
+++ src/products/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    # description = models.TextField(null=True)
+    description = models.TextField(null=True) # null=True is an null value in db
+    short_description = models.CharField(max_length=220)
     price = models.DecimalField(max_digits=20, decimal_places=2)
