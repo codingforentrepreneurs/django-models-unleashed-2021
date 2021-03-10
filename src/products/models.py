30a15dc058ec76f146dc490785172a7d5723c3ae
CFE <hello@teamcfe.com>
2021-03-10 13:56:52 -0800

    8 - Delete a Field

--- src/products/models.py
+++ src/products/models.py
@@ -4,5 +4,4 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(null=True) # null=True is an null value in db
-    short_description = models.CharField(max_length=220)
     price = models.DecimalField(max_digits=20, decimal_places=2)
