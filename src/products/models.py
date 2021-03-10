ec848bacd7727d62f1c7e0d2045b114dfb86bb53
CFE <hello@teamcfe.com>
2021-03-10 13:35:24 -0800

    5 - Basics of Django Model Field Types

--- src/products/models.py
+++ src/products/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.TextField()
-    price = models.DecimalField()
+    title = models.CharField(max_length=None)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
