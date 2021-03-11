48d53a1173efbcd34010804aa83b55b8d8477179
CFE <hello@teamcfe.com>
2021-03-11 12:03:47 -0800

    11 & 12 - Model Field Validation

--- src/products/models.py
+++ src/products/models.py
@@ -4,6 +4,9 @@ from .validators import validate_blocked_words
 
 # Create your models here.
 class Product(models.Model):
+    """
+    admin.site.register(Product)
+    """
     title = models.CharField(max_length=120, validators=[validate_blocked_words])
     description = models.TextField(null=True) # null=True is an null value in db
     price = models.DecimalField(max_digits=20, decimal_places=2)
