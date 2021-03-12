6ae92c47125100329422f24eeb9747c367b09f61
CFE <hello@teamcfe.com>
2021-03-12 13:33:05 -0800

    18 - Model Meta Options

--- src/products/models.py
+++ src/products/models.py
@@ -12,7 +12,6 @@ class Product(models.Model):
         PUBLISH = 'PU', 'Published'
         DRAFT = 'DR', 'Draft'
         PRIVATE = 'PR', 'Private'
-    
     title = models.CharField(max_length=120, validators=[validate_blocked_words])
     description = models.TextField(null=True) # null=True is an null value in db
     price = models.DecimalField(max_digits=20, decimal_places=2)
@@ -20,6 +19,12 @@ class Product(models.Model):
     publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True) # auto set when the state changes to `PUBLISH`
     timestamp = models.DateTimeField(auto_now_add=True) # auto set when this object was created
     updated = models.DateTimeField(auto_now=True) # auto set when this object was lasted saved
+    class Meta:
+        ordering = ['-updated', '-timestamp']
+        # verbose_name = 'Product' 
+        # verbose_name_plural =  'Products'
+        # unique_together = [['title', 'order']]
+        # db_table = '<appname>_<modelname>' # 'products_mycoolproduct'
 
     def save(self, *args, **kwargs):
         validate_blocked_words(self.title)
@@ -37,8 +42,3 @@ class Product(models.Model):
     def is_published(self):
         publish_timestamp = self.publish_timestamp
         return self.state_is_published and publish_timestamp < timezone.now()
-
-    class Meta:
-        ordering = ['-publish_timestamp', '-updated', '-timestamp']
-        get_latest_by = ['-publish_timestamp', '-updated', '-timestamp']
-        # products_product
