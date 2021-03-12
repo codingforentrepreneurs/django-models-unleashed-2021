c7bd5d68718bfb06542e1900bbf69ceca2ffb867
CFE <hello@teamcfe.com>
2021-03-12 12:31:58 -0800

    16 - DateField & DateTimeField

--- src/products/models.py
+++ src/products/models.py
@@ -1,5 +1,6 @@
+# import datetime
 from django.db import models
-
+from django.utils import timezone
 from .validators import validate_blocked_words
 
 # (DB_VALUE, USER_FACING_VALUE)
@@ -16,14 +17,26 @@ class Product(models.Model):
     state = models.CharField(max_length=2, default=ProductStateOptions.DRAFT, choices=ProductStateOptions.choices)
     description = models.TextField(null=True) # null=True is an null value in db
     price = models.DecimalField(max_digits=20, decimal_places=2)
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True) # auto set when the state changes to `PUBLISH`
+    timestamp = models.DateTimeField(auto_now_add=True) # auto set when this object was created
+    updated = models.DateTimeField(auto_now=True) # auto set when this object was lasted saved
 
     def save(self, *args, **kwargs):
         validate_blocked_words(self.title)
+        if self.state_is_published and self.publish_timestamp is None:
+            self.publish_timestamp = timezone.now()
+        else:
+            self.publish_timestamp = None
         super().save(*args, **kwargs)
 
     @property
-    def is_published(self):
+    def state_is_published(self):
         return self.state == self.ProductStateOptions.PUBLISH
+    
+    @property
+    def is_published(self):
+        publish_timestamp = self.publish_timestamp
+        return self.state_is_published and publish_timestamp < timezone.now()
 
     """
     def clean(self):
